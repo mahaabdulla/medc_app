b72c54f0dc306a33be3c35ef2359266bbf2b031e
mahaabdulla <mahaabdulla87@gmail.com>
2025-11-21 16:15:18 +0300

    add fontWieght class

--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:medbok/core/theming/colors.dart';
+import 'package:medbok/core/theming/font_wieght_helper.dart';
 
 class AppTextStyles {
   static TextStyle font24Black70Weight = TextStyle(
     fontSize: 24.sp,
     color: Colors.black,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font24BlueBlod = TextStyle(
+    fontSize: 24.sp,
+    color: ColorsManager.mainBlue,
+    fontWeight: FontWeightHelper.bold,
   );
 
   static TextStyle font32BlueBold = TextStyle(
     fontSize: 32.sp,
     color: ColorsManager.mainBlue,
-    fontWeight: FontWeight.bold,
+    fontWeight: FontWeightHelper.bold,
   );
   static TextStyle font16WhiteSemiBokd = TextStyle(
     fontSize: 16.sp,
     color: Colors.white,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeightHelper.bold,
   );
   static TextStyle font13GeryReguler = TextStyle(
     fontSize: 13.sp,
     color: ColorsManager.gery,
-    fontWeight: FontWeight.normal,
+    fontWeight: FontWeightHelper.regular,
   );
 }
--- lib/features/login/views/login_screen.dart
+++ lib/features/login/views/login_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:medbok/core/helpers/spacing.dart';
+import 'package:medbok/core/theming/styles.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('lOGIN')));
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Welcome Back', style: AppTextStyles.font24BlueBlod),
+                verticalSpace(20),
+                Text(
+                  'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
+                  style: AppTextStyles.font13GeryReguler.copyWith(
+                    fontSize: 14.sp,
+                  ),
+                ),
+                verticalSpace(36),
+                Form(child: Column(children: [TextFormField()])),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
